1a5c4ab5ccb5cda758c01a51c1efcc6c2234931d
Luis Villén <l.villenjaldon@gmail.com>
2020-11-09 17:35:39 +0100

    Funciones clásicas

---
+++ p1.py
@@ -0,0 +1,17 @@
+def sumaTodos(limitTo):
+    resultado = 0
+    for i in range(0, limitTo+1):
+        resultado += i
+    
+    return resultado
+
+print(sumaTodos(100))
+
+def sumaTodosLosCuadrados(limitTo):
+    resultado = 0
+    for i in range(limitTo+1):
+        resultado += i*i
+    
+    return resultado
+
+print(sumaTodosLosCuadrados(3))
